14f21fc2299fdc7cbc7f8ad87fca2c55a04ea8bd
Muhammad Abdullah Waheed <mabdullahwaheed127@gmail.com>
2024-10-29 14:10:11 +0500

    add three sections (Support ,Hosting, Airbnb) in the footer.

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./card.css";
+
 const Card = ({ card }) => {
     return (
         <div className="card-container">
             <div className="card-image-container">
                 {card.imgSrc.map((src, i) => (
-                    <img src={src} className="card-image" alt={`slide ${i}`} />
+                    <img key={i} src={src} className="card-image" alt={`slide ${i}`} />
                 ))}
             </div>
-
             <div className="card-details">
-                <h5 className="card-heading">{card.heading}</h5>
-                <p className="discription">{card.discription}</p>
-                <h4 className="card-heading">{card.isSold}</h4>
+                {/* <h5 className="card-heading">{card.heading}</h5> */}
+                <h5 className="card-heading">Abdullah</h5>
+
+                <p className="card-description">Abdullah</p>
+                {card.isSold && <h4 className="card-status">Sold</h4>}
             </div>
         </div>
     );
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import './Footer.css'; // Assuming you'll use CSS for styling
+import './footer.css'; // Assuming you'll use CSS for styling
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer-section-1">
-                <h2>Inspiration for future getaways</h2>
-                <div className="footer-links">
-                    <ul className="footer-section-1-ul">
-                        <li>Popular</li>
-                        <li>Arts & culture</li>
-                        <li>Outdoors</li>
-                        <li>Mountains</li>
-                        <li>Beach</li>
-                        <li>Unique stays</li>
-                        <li>Things to do</li>
-                        <li>Travel tips & inspiration</li>
-                        <li>Airbnb-friendly apartments</li>
-                    </ul>
-                </div>
-            </div>
+            <section class="hosting-section">
+                <h3>Support</h3>
+                <ul>
+                    <li><a href="/Abdullah">Airbnb your home</a></li>
+                    <li><a href="/Abdullah">AirCover for Hosts</a></li>
+                    <li><a href="/Abdullah">Hosting resources</a></li>
+                    <li><a href="/Abdullah">Community forum</a></li>
+                    <li><a href="/Abdullah">Hosting responsibly</a></li>
+                    <li><a href="/Abdullah">Airbnb-friendly apartments</a></li>
+                    <li><a href="/Abdullah">Join a free Hosting class</a></li>
+                    <li><a href="/Abdullah">Explore co-hosting</a></li>
+                </ul>
+            </section>
+
+            <section class="hosting-section">
+                <h3 class="section-title">Hosting</h3>
+                <ul class="link-list">
+                    <li><a href="/host/homes?from_footer=1">Airbnb your home</a></li>
+                    <li><a href="/aircover-for-hosts">AirCover for Hosts</a></li>
+                    <li><a href="/resources">Hosting resources</a></li>
+                    <li><a href="/help/community?s=footer">Community forum</a></li>
+                    <li><a href="/help/responsible-hosting">Hosting responsibly</a></li>
+                    <li><a href="/airbnb-friendly">Airbnb-friendly apartments</a></li>
+                    <li><a href="/ambassadors/joinaclass">Join a free Hosting class</a></li>
+                    <li><a href="/host/co-hosts">Find a co-host</a></li>
+                </ul>
+            </section>
+
+            <section class="hosting-section">
+                <h3>Airbnb</h3>
+                <ul class="link-list">
+                    <li><a href="/press/news">Newsroom</a></li>
+                    <li><a href="/release">New features</a></li>
+                    <li><a href="/careers">Careers</a></li>
+                    <li><a href="https://investors.airbnb.com">Investors</a></li>
+                    <li><a href="/giftcards">Gift cards</a></li>
+                    <li><a href="https://www.airbnb.org?locale=en">Airbnb.org emergency stays</a></li>
+                </ul>
+            </section>
+
         </footer>
     );
 };
